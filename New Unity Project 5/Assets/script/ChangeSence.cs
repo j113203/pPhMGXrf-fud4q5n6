2df76a24d29c62b6662491c0379718fab9779e70
kwanyuhung <kwanyuhung@gmail.com>
2015-07-31 23:06:13 +0800

    Revert "2015年07月31日"
    This reverts commit 2644c8ab8f019ba84dd2dec61fc9be62b77ca093.

--- New Unity Project 5/Assets/script/ChangeSence.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ChangeSence : MonoBehaviour {
-	
-	public void ChangeToScene (string scenceToChangeTo) {
-		Application.LoadLevel (scenceToChangeTo);
-	}
-}
